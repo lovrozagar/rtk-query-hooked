c00eec5d1a172343f8db97705322dc9d7c4bb159
Lovro Žagar <157136236+ht-lovrozagar@users.noreply.github.com>
2024-06-20 14:40:30 +0200

    feat: add typescript and lib types

---
+++ src/types/types.ts
@@ -0,0 +1,81 @@
+import {
+  UseMutation,
+  UseQuery,
+} from "@reduxjs/toolkit/dist/query/react/buildHooks";
+import {
+  BaseQueryFn,
+  MutationDefinition,
+  QueryDefinition,
+} from "@reduxjs/toolkit/query";
+
+/* QUERY */
+
+type RTKQueryAnyHook = UseQuery<any>;
+
+type RTKQueryHook<
+  QueryArg,
+  BaseQuery extends BaseQueryFn,
+  ResultType
+> = UseQuery<QueryDefinition<QueryArg, BaseQuery, string, ResultType, string>>;
+
+const useQueryWrapper = <QueryArg, BaseQuery extends BaseQueryFn, ResultType>(
+  useQueryHook: RTKQueryHook<QueryArg, BaseQuery, ResultType>
+) => {
+  // this is only used for advanced typing, hook should not ever be called
+  return useQueryHook({} as any);
+};
+
+type UseQueryData<
+  QueryArg,
+  BaseQuery extends BaseQueryFn,
+  ResultType
+> = ReturnType<typeof useQueryWrapper<QueryArg, BaseQuery, ResultType>>["data"];
+
+/* MUTATION */
+
+type RTKMutationAnyHook = UseMutation<any>;
+
+type RTKMutationHook<
+  QueryArg,
+  BaseQuery extends BaseQueryFn,
+  ResultType
+> = UseMutation<
+  MutationDefinition<QueryArg, BaseQuery, string, ResultType, string>
+>;
+
+const useMutationWrapper = <
+  QueryArg,
+  BaseQuery extends BaseQueryFn,
+  ResultType
+>(
+  useMutationHook: RTKMutationHook<QueryArg, BaseQuery, ResultType>
+) => {
+  // this is only used for advanced typing, hook should not ever be called
+  return useMutationHook({} as any);
+};
+
+type UseMutationData<
+  QueryArg,
+  BaseQuery extends BaseQueryFn,
+  ResultType
+> = ReturnType<
+  typeof useMutationWrapper<QueryArg, BaseQuery, ResultType>
+>[1]["data"];
+
+/* HOOKED EVENTS */
+
+type RTKHookedEvents<T> = {
+  onSettled?: () => void;
+  onSuccess?: (data: NonNullable<T>) => void;
+  onError?: (error: unknown) => void;
+};
+
+export type {
+  RTKHookedEvents,
+  RTKMutationAnyHook,
+  RTKMutationHook,
+  RTKQueryAnyHook,
+  RTKQueryHook,
+  UseMutationData,
+  UseQueryData,
+};
